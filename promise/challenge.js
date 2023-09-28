2518e63f0a6a9f0ce557f4be16fa7230c2af4414
Leodan Valda <leo_1998@live.cl>
2023-09-28 12:17:30 -0300

    fetch promise

--- promise/challenge.js
+++ promise/challenge.js
@@ -3,7 +3,25 @@ import fetch from "node-fetch";
 const API = 'https://api.escuelajs.co/api/v1';
 
 const fetchData = (url_api) =>  {return fetch(url_api)}
+// fetchData(`${API}/products`)
+//     .then(respuesta => respuesta.json())
+//     .then(productos => console.log(productos))
+//     .catch(error => console.log(error))
+
 fetchData(`${API}/products`)
     .then(respuesta => respuesta.json())
-    .then(productos => console.log(productos))
-    .catch(error => console.log(error))
+    .then(productos => {
+        fetchData(`${API}/products/${productos[0].id}`)
+        .then(respuesta => respuesta.json())
+        .then(producto => {
+            fetchData(`${API}/categories/${producto.category.id}`)
+            .then(respuesta => respuesta.json())
+            .then(categoria => {
+                console.log(productos[0].title);
+                console.log(producto.title);
+                console.log(categoria.name);
+            })
+            .catch(error => console.log(error))
+            .finally(()=> console.log('finalizado'));
+            })
+        })
